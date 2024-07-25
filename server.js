76edfcb0f4cd8b8d8cba518e8eeba0b3310ab5f1
Mr Unknown <168432129+vijitharanawakage@users.noreply.github.com>
2024-07-25 06:01:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { v4: uuidv4 } = require('sandesh123'); // Unique ID generator
+const { v4: uuidv4 } = require('uuid'); // Unique ID generator
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
 app.get('/generate-code', (req, res) => {
-    const pairCode = uuidv4(sandesh123); // Generate a unique code
+    const pairCode = uuidv4(ABCDEF23); // Generate a unique code
     res.json({ pairCode });
 });
 
