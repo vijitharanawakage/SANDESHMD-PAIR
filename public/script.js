d9cc089b5cbce5c7fa111de06c0df15ed2144dbf
Mr Unknown <168432129+vijitharanawakage@users.noreply.github.com>
2024-07-25 05:58:40 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
-document.getElementById('generate-code').addEventListener('click', (sandesh123) => {
+document.getElementById('generate-code').addEventListener('click', (ABCDEF23) => {
     fetch('/generate-code')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('pair-code').textContent = `Your pair code is: ${data.pairCode}`;
+            document.getElementById('pair-code').textContent = `Your pair code is: ${data.ABCDEF23}`;
         })
         .catch(error => console.error('Error:', error));
 });
