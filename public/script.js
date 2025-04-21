b827c472d192b210a7e76d68fcce986231696ade
Mr Unknown <168432129+vijitharanawakage@users.noreply.github.com>
2025-04-21 09:19:52 +0530

    Delete public directory

--- public/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('generate-code').addEventListener('click', (ABCDEF23) => {
-    fetch('/generate-code')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('pair-code').textContent = `Your pair code is: ${data.ABCDEF23}`;
-        })
-        .catch(error => console.error('Error:', error));
-});
